fa7a8ab90600d2b1baa54deb8a395183c4bf168e
hongbo zhang <hbzhang@vt.edu>
2020-06-12 12:11:29 -0400

    change

--- kinect_learning.py
+++ kinect_learning.py
@@ -52,7 +52,7 @@ def load_data(file_name, collection, noise):
 		if yi == 2:
 			if noise == True:
 				count += 1
-				if count > 1500:
+				if count > 300:
 					continue
 				else:
 					yi = random.randint(0,1)
