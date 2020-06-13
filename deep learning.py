661958a4836b1d708a262f7d3fcab1d48167dbf1
hongbo zhang <hbzhang@vt.edu>
2020-06-12 23:59:16 -0400

    change

--- deep learning.py
+++ deep learning.py
@@ -47,7 +47,7 @@ class CyclicLR(_LRScheduler):
     def __init__(self, optimizer, schedule, last_epoch=-1):
         assert callable(schedule)
         self.schedule = schedule
-        super().__init__(optimizer, last_epoch)
+        super(CyclicLR,self).__init__(optimizer, last_epoch)
 
     def get_lr(self):
         return [self.schedule(self.last_epoch, lr) for lr in self.base_lrs]
